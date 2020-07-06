2812fbe3dc8bb0b3cf9f2bda0e43626054e25220
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2020-07-06 18:23:07 -0400

    updates

---
+++ src/main/java/hbcu/stay/ready/mastering_loops/MainApp.java
@@ -0,0 +1,9 @@
+package hbcu.stay.ready.mastering_loops;
+
+import java.util.Scanner;
+
+
+public class MainApp {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/hbcu/stay/ready/mastering_loops/TestSuite.java
@@ -0,0 +1,15 @@
+package hbcu.stay.ready.mastering_loops;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        NumberUtilitiesTest.class,
+        TriangleUtilitiesTest.class,
+        TableUtilitiesTest.class,
+})
+public class TestSuite {
+}
